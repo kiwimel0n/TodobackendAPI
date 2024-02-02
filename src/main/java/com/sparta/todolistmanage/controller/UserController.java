053696ab119dc981880caa4dc9dbd02354f15bf2
kiwimel0n <alstkd8580@naver.com>
2024-02-02 12:07:50 +0900

    회원가입 API 구현

--- src/main/java/com/sparta/todolistmanage/controller/UserController.java
+++ src/main/java/com/sparta/todolistmanage/controller/UserController.java
@@ -9,12 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Slf4j
 @Controller
