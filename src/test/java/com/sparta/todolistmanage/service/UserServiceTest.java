625980d0577c6d89c713946cf54c39448c5fd172
kiwimel0n <alstkd8580@naver.com>
2024-02-21 11:42:25 +0900

    # test : 테스트 (테스트 코드 추가 : userService signup 정상적인 회원가입 테스트 및 중복된 username 회원가입 테스트)

---
+++ src/test/java/com/sparta/todolistmanage/service/UserServiceTest.java
@@ -0,0 +1,79 @@
+package com.sparta.todolistmanage.service;
+
+import com.sparta.todolistmanage.dto.SignupRequestDto;
+import com.sparta.todolistmanage.entity.User;
+import com.sparta.todolistmanage.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+class UserServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+    @Spy
+    PasswordEncoder passwordEncoder;
+    @InjectMocks
+    UserService userService;
+
+
+
+
+    @Test
+    @DisplayName("정상적인 회원가입")
+    void test1() {
+        //given
+        String username = "Bob4";
+        String password = "1234567@";
+
+        when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        //when
+        SignupRequestDto signupRequestDto = new SignupRequestDto(username, password);
+        userService.signup(signupRequestDto);
+
+        //then
+        verify(userRepository, times(1)).save(any(User.class));
+        verify(passwordEncoder, times(1)).encode(any(String.class));
+
+    }
+
+    @Test
+    @DisplayName("중복된 username 회원가입")
+    void test2() {
+        //given
+       String username = "Bob5";
+       String password = "1234567@";
+
+       when(userRepository.findByUsername(username)).thenReturn(Optional.of(new User()));
+        //when
+       Throwable exception = assertThrows(IllegalArgumentException.class, ()->{
+            SignupRequestDto signupRequestDto = new SignupRequestDto(username, password);
+            userService.signup(signupRequestDto);
+        });
+
+
+       //then
+        assertEquals("중복된 사용자가 존재 합니다.",exception.getMessage());
+
+
+
+
+    }
+
+
+}
